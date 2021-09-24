b8dfc3db86f328d52e6d0697de7cf4e93a693d7f
pranav9623 <91110233+pranav9623@users.noreply.github.com>
2021-09-24 09:35:49 +0530

    Add files via upload

---
+++ comparejson.js
@@ -0,0 +1,26 @@
+"use strict";
+var obj1 = { name: "Person 1", age: 5 };
+var obj2 = { age: 5, name: "Person 1" };
+
+const deepCompare = (arg1, arg2) => {
+  if (
+    Object.prototype.toString.call(arg1) ===
+    Object.prototype.toString.call(arg2)
+  ) {
+    if (
+      Object.prototype.toString.call(arg1) === "[object Object]" ||
+      Object.prototype.toString.call(arg1) === "[object Array]"
+    ) {
+      if (Object.keys(arg1).length !== Object.keys(arg2).length) {
+        return false;
+      }
+      return Object.keys(arg1).every(function (key) {
+        return deepCompare(arg1[key], arg2[key]);
+      });
+    }
+    return arg1 === arg2;
+  }
+  return false;
+};
+
+console.log(deepCompare(obj1, obj2));
